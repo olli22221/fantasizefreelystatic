9a3868487bc385bb2b9c740f934e2d5bed846185
Oliver Pohl <olli22221@web.de>
2022-11-06 18:30:33 +0100

    added surveyswap links

--- src/components/surveyToken.js
+++ src/components/surveyToken.js
@@ -33,7 +33,11 @@ function SurveyToken() {
         <div>
             
             <Popup open={open} closeOnDocumentClick onClose={closeModal}>   
-                 <div className="modal">     <Button>Teeeesssssttt</Button>     <a className="close" onClick={closeModal}>  
+                 <div className="modal">      <p>The following code gives you credits that can be used to get free research participants at SurveySwap.io. 
+ 
+ Go to: <a href="https://surveyswap.io/sr/0Q8B-ZY5L-C1KT">https://surveyswap.io/sr/0Q8B-ZY5L-C1KT </a>
+  
+ Or, alternatively, enter the code manually: 0Q8B-ZY5L-C1KT</p>      <a className="close" onClick={closeModal}>  
                       &times;          </a>          
                         </div>  
             </Popup>
