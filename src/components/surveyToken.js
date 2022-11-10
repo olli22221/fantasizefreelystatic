ccc293138b2b930fbbb012418162c53bc5e1190b
Oliver Pohl <olli22221@web.de>
2022-11-10 19:34:07 +0100

    added survey swap link2

--- src/components/surveyToken.js
+++ src/components/surveyToken.js
@@ -33,11 +33,9 @@ function SurveyToken() {
         <div>
             
             <Popup open={open} closeOnDocumentClick onClose={closeModal}>   
-                 <div className="modal">      <p>The following code gives you credits that can be used to get free research participants at SurveySwap.io. 
- 
- Go to: <a href="https://surveyswap.io/sr/0Q8B-ZY5L-C1KT">https://surveyswap.io/sr/0Q8B-ZY5L-C1KT </a>
+                 <div className="modal">      <p>
   
- Or, alternatively, enter the code manually: 0Q8B-ZY5L-C1KT</p>      <a className="close" onClick={closeModal}>  
+ Enter this Completion Code on Profilic: C14RW7J7</p>      <a className="close" onClick={closeModal}>  
                       &times;          </a>          
                         </div>  
             </Popup>
